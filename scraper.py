b1995b5042bdfc3c4e110ac161c15de7a4fbe0eb
kian79 <kiankr79@gmail.com>
2020-04-30 05:20:58 +0430

    successfully got data and made csv!

--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup as Soup
 import re, urllib.request
 from pathlib import Path
-import requests
+import csv
+from tqdm import tqdm
 
 
 def removeWeirdChars(text0):
@@ -36,46 +37,61 @@ def get_html_files(path: str) -> list:
     htmls = []
     for file in files_path:
         htmls.append(open(file, 'r').read())
+    print(len(htmls))
     return htmls
 
 
-def process_data(a_soup: Soup):
-    my_dict = {}
-    joined = False
-    messages = a_soup.find_all('div', class_='message default clearfix')
-    if not messages:
-        joined = True
-        messages = a_soup.find_all('div', class_='message default clearfix joined')
-    for msg in messages:
-        my_dict = extract_data(msg, joined)
+def process_data(a_list: list):
+    my_list = []
+    my_dict={}
+    for i in tqdm(range(len(a_list))):
+        a_soup = Soup(a_list[i],'html.parser')
+        my_dict={}
+        messages = a_soup.find_all('div', class_=['message default clearfix','message default clearfix joined'])
+        username = ""
+        for msg in messages:
+            my_dict = extract_data(msg)
+            if my_dict['user_id']:
+                username = my_dict['user_id']
+            else:
+                my_dict['user_id'] = username
+            my_list.append(my_dict)
+    with open('tele_data.csv', 'a') as my_csv:
+        my_writer = csv.DictWriter(my_csv, fieldnames=my_dict.keys())
+        my_writer.writeheader()
+        my_writer.writerows(my_list)
 
 
+def extract_data(message):
+    date, time = message.find('div', class_="pull_right date details").get('title').split()
+    msg_id = message.get('id')[7:]
+    is_reply = message.find('div', class_="reply_to details")
+    if is_reply:
+        is_reply = is_reply.find('a').get('href').split('_')[2][7:]
 
-
-def extract_data(message, is_joined, a_dict):
-    if not is_joined:
-        date, time = message.find('div', class_="pull_right date details").get('title').split()
-        msg_id = message.get('id')[7:]
-        is_reply = message.find('div', class_="reply_to details")
-        if is_reply:
-            is_reply = is_reply.find('a').get('href').split('_')[2][7:]
-        user = message.find('div', class_="from_name").text
-        text = message.find('div', class_='text')
-        a_dict = dict(
-            msg_id=msg_id,
-            user_id=user,
-            text=text,
-            date=date,
-            time=time,
-            reply_to=is_reply
-        )
+    text = message.find('div', class_='text')
+    if not text:
+        text = "NO TEXT"
     else:
-        a_dict['text'] = a_dict['text'] + '\n' + message.find('div', class_='text')
+        text=text.text
+    user = message.find('div', class_="from_name")
+    if user:
+        user=user.text
+    a_dict = dict(
+        msg_id=msg_id,
+        user_id=user,
+        text=text,
+        date=date,
+        time=time,
+        reply_to=is_reply
+    )
 
     return a_dict
 
-# dir_path = input()
-# my_html = '\n'.join(get_html_files(dir_path))
-# my_html = removeWeirdChars(my_html)
-my_soup = Soup(open('messages34.html', 'r').read(), 'html.parser')
-process_data(my_soup)
+
+dir_path = input()
+print(0)
+my_html = get_html_files(dir_path)
+print(1)
+process_data(my_html)
+print(" تموم شد به حق علی")
